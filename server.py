5fedf3c5ad7914b87a39d51b55b2ec46e69524fb
ndrishinski <ndrishinski@yahoo.com>
2023-06-21 20:42:37 -0400

    created hash_table.py

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ class User(db.Model):
   email = db.Column(db.String(50))
   address = db.Column(db.String(200))
   phone = db.Column(db.String(50))
-  posts = db.relationship("BlogPost")
+  posts = db.relationship("BlogPost", cascade="all, delete")
 
 class BlogPost(db.Model):
   __tablename__ = 'blog_post'
@@ -115,7 +115,10 @@ def get_one_user(user_id):
 
 @app.route('/user/<user_id>', methods=["DELETE"])
 def delete_user(user_id):
-  pass
+  user = User.query.filter_by(id=user_id).first()
+  db.session.delete(user)
+  db.session.commit()
+  return jsonify({}), 200
 
 @app.route('/blog_post/<user_id>', methods=["POST"])
 def create_blog_post(user_id):
